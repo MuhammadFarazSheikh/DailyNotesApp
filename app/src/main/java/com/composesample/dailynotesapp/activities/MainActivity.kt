c7acfeaf41f523809856d5e990b2d700d7bebf9f
Muhammad Faraz Sheikh <faraz.sheikh@prvidr.com>
2022-10-31 17:31:42 +0500

    Code Push

---
+++ app/src/main/java/com/composesample/dailynotesapp/activities/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.composesample.dailynotesapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.composesample.dailynotesapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/composesample/dailynotesapp/activities/SplashScreen.kt
@@ -0,0 +1,78 @@
+package com.composesample.dailynotesapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.composesample.dailynotesapp.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(
+            ComposeView(this).apply {
+                setContent {
+                    splashScreenUI()
+                }
+            }
+        )
+        startTimer()
+    }
+
+    /*START 2 SECONDS TIMER FOR SPLASH SCREEN*/
+    private fun startTimer()
+    {
+        object:CountDownTimer(2000,1000)
+        {
+            override fun onTick(p0: Long) {
+            }
+
+            override fun onFinish() {
+                checkUserSession()
+            }
+        }.start()
+    }
+
+    /*CHECK USER SESSION IF ALREADY LOGIN SO OPEN MAIN SCREEN*/
+    /*OTHERWISE OPEN LOGIN OR SIGNUP SCREEN*/
+    private fun checkUserSession()
+    {
+
+    }
+
+    @Preview
+    @Composable
+    fun splashScreenUI()
+    {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .background(color = Color.LightGray),
+            content ={
+                Text(
+                    stringResource(R.string.text_splash_title),
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+            },
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
+    }
+}
